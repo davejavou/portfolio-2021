6b43b7c7409753dde647b7c841522a6d65eac53c
Dave <github@davecutter.com>
2021-07-14 23:26:47 +1000

    Working on lightbox and routes

--- components/projects.js
+++ components/projects.js
@@ -1,6 +1,8 @@
+import { useRef, useEffect } from 'react';
+import { useRouter } from 'next/router'
 import Slider from 'react-slick'
 import { portfolio, photography } from "../public/content"
-import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox'
+import { SRLWrapper, useLightbox } from 'simple-react-lightbox'
 
 function NextArrow({ className, onClick }) {
   return (
@@ -25,14 +27,14 @@ function PrevArrow({ className, onClick }) {
 }
 
 export default function Projects({ content }) {
-
   /* Data can only be projects or photos */
   const projects = (content === 'photography') ? photography : portfolio
 
   const imgPath = '/content-images/'
 
+  // Configure Carosuel
   // https://react-slick.neostack.com/
-  var sliderSettings = {
+  const sliderSettings = {
     lazyLoad: true,
     dots: true,
     infinite: true,
@@ -44,52 +46,100 @@ export default function Projects({ content }) {
     prevArrow: <PrevArrow />
   }
 
-  // WIP: Configuring Lightbox
+  // Configure Lightbox
   // https://github.com/michelecocuccio/simple-react-lightbox
+  // Currently this works by surrounding the root <App /> with <SimpleReactLightbox>, there should be a better way - having the whole lightbox used in this file would be best.
+  const { openLightbox } = useLightbox()
 
+  const lightboxSettings = {
+    settings: {
+      lightboxTransitionSpeed: 0.3
+    },
+    thumbnails: {
+      showThumbnails: false,
+    }
+  }
+
+  // Route to Project and/or slide
+  const { query } = useRouter()
+  let targetProject = null
+  let targetSlider = null
+  let targetSlide = null
+
+  // Projects & Slides rendering data
+  let slideglobalIndex = -1; // Index of all slides on page across all projects. Start with no slides (-1, because one slide would be index 0)
+  // This map is adding ui information to the projects data, and outputting a flat list of all slides for the ligtbox
+  const lightboxElements = projects.map(project => {
+    project.projectRef = useRef()
+    project.sliderRef = useRef()
+    // Project Query
+    if (project.id == query.p) {
+      targetProject = project.projectRef
+    }
+    return project.slides.map((slide, index) => {
+      // Add Slide Indexes, one index for the whole page of slides, and one index for this project slider
+      slideglobalIndex++
+      slide.globalIndex = slideglobalIndex
+      slide.sliderIndex = index
+      // Slide query. If exists, will overrule any project query.
+      if (slide.id == query.s) {
+        targetProject = project.projectRef
+        targetSlider = project.sliderRef
+        targetSlide = slide.sliderIndex
+      }
+      return {
+        caption: `${project.client}: ${project.title}`,
+        src: `${imgPath}Zoom-${project.id}-${slide.id}.png`
+      }
+    })
+  }).flat()
+
+  // Scroll to target project and/or set the slide
+  useEffect(() => {
+    if (targetProject) {
+      targetProject.current.scrollIntoView({ behavior: 'smooth' })
+    }
+    if (targetSlider && targetSlide) {
+      targetSlider.current.slickGoTo(targetSlide)
+    }
+  }, [targetProject, targetSlider, targetSlide])
 
   return (
-    <SimpleReactLightbox>
-    {projects.map(({id, client, location, year, title, description, credit, color, slides}) => (
-      <div key={`project-${id}`} id={`project-${id}`} style={{background: color}}>
+    <>
+      {/* Carosuel */}
+      {projects.map(({id, client, location, year, title, description, credit, color, slides, projectRef, sliderRef}) =>
+        <div key={`project-${id}`} ref={projectRef} style={{background: color}}>
 
-        <div className="flex justify-between text-white text-lg font-light uppercase p-8 pb-1">
-          <span>{client}</span>
-          <span>{location}, {year}</span>
-        </div>
+          <div className="flex justify-between text-white text-lg font-light uppercase p-8 pb-1">
+            <span>{client}</span>
+            <span>{location}, {year}</span>
+          </div>
 
-        <Slider {...sliderSettings}>
-          {/* WIP: Could be an image or youtube video, must click to lightbox, and arrows to go through set  */}
-          {slides.map( slide =>
-            <div key={`${id}-${slide.id}`}> {/* This container div will be styled by react-slick carosuel. Do not style or add classes */}
-              <div className="w-full">
-                <img className="object-contain h-project-row max-h-max-project-row mx-auto" src={`${imgPath}Slide-${id}-${slide.id}.png`} alt={`Slide-${id}-${slide.id}`} />
+          <Slider ref={sliderRef} {...sliderSettings}>
+            {/* WIP: Could be an image or youtube video, must click to lightbox, and arrows to go through set  */}
+            {slides.map(slide =>
+              <div key={`${id}-${slide.id}`}> {/* This container div will be styled by react-slick carosuel. Do not style or add classes */}
+                <div className="w-full">
+                  <img
+                    className="object-contain h-project-row max-h-max-project-row mx-auto"
+                    src={`${imgPath}Slide-${id}-${slide.id}.png`} alt={`Slide-${id}-${slide.id}`}
+                    onClick={() => openLightbox(slide.globalIndex)}
+                  />
+                </div>
               </div>
-            </div>
-          )}
-        </Slider>
+            )}
+          </Slider>
 
-        <div className="text-white py-8 px-10">
-            <h2 className="text-2xl font-serif pb-2">{title}</h2>
-            <p>{description}</p>
+          <div className="text-white py-8 px-10">
+              <h2 className="text-2xl font-serif pb-2">{title}</h2>
+              <p>{description}</p>
+          </div>
+
         </div>
+      )}
 
-        {/* Dev Helper */}
-        {/*
-        <div className="absolute top-4 left-4 w-3/4 text-xs bg-gray-100 bg-opacity-50 p-10">
-          id: {id} <br />
-          client: {client} <br />
-          location: {location} <br />
-          year: {year} <br />
-          title: {title} <br />
-          description: {description} <br />
-          credit: {credit} <br />
-          color: {color}
-        </div>
-        */}
-
-      </div>
-    ))}
-    </SimpleReactLightbox>
+      {/* Lightbox */}
+      <SRLWrapper options={lightboxSettings} elements={lightboxElements} />
+    </>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import '../styles/slick.1.6.0.min.css'
+import SimpleReactLightbox from 'simple-react-lightbox'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <SimpleReactLightbox><Component {...pageProps} /></SimpleReactLightbox>
 }
