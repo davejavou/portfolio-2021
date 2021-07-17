bc3536d792d2228c1d24b80248699ba9b9545ffa
Dave <github@davecutter.com>
2021-07-17 15:33:50 +1000

    Add Portfolio Content

--- public/content.js
+++ public/content.js
@@ -1,126 +1,335 @@
 export const portfolio = [
   {
-    "id": "1",
-    "client": "Mod.io",
-    "location": "Melbourne",
-    "year": 2021,
-    "title": "Mod.io Home Page",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "color": "#111",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-1-1.png", "zsrc": "/content-images/Zoom-1-1.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-1-2.png", "zsrc": "/content-images/Zoom-1-2.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-1-3.png", "zsrc": "/content-images/Zoom-1-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-1-4.png", "zsrc": "/content-images/Zoom-1-4.png" }
-    ]
+    client: "Mod.io",
+    location: "Melbourne",
+    year: 2021,
+    title: "Mod.io Home Page",
+    description:
+      "Designing ONE homepage for both gamers and game developers, as well as for desktop, tablet, and mobile, all in one - to serve Mod.io's groundbreaking technology in mod development and distribution across gaming platforms!",
+    color: "#44BFD5",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/modio-home-01.png",
+        zsrc: "/content-images/modio-home-01-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/modio-home-02.png",
+        zsrc: "/content-images/modio-home-02-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/modio-home-03.png",
+        zsrc: "/content-images/modio-home-03-zoom.png",
+      },
+    ],
   },
   {
-    "id": "2",
-    "client": "Sports Performance Tracking",
-    "location": "Melbourne",
-    "year": 2020,
-    "title": "Game Traka",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "credit":"Video by Jon",
-    "color": "#222",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-2-1.png", "zsrc": "/content-images/Zoom-2-1.png" },
-      { "type": "youtube", "ssrc": "https://www.youtube-nocookie.com/embed/xshEZzpS4CQ?modestbranding=1", "zsrc": "https://www.youtube.com/watch?v=xshEZzpS4CQ&modestbranding=1" },
-      { "type": "image", "ssrc": "/content-images/Slide-2-3.png", "zsrc": "/content-images/Zoom-2-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-2-4.png", "zsrc": "/content-images/Zoom-2-4.png" }
-    ]
+    client: "Sports Performance Tracking",
+    location: "Melbourne",
+    year: 2020,
+    title: "Game Traka",
+    description:
+      "A complete redesign and redevelopment of Sports Performance Tracking's athlete analytics and performance improvement software system, 'Game Traka', utilizing data from GPS and accelerometer tracking devices worn by athletes all over the world and designed for both sports scientists as well as coaches and players to take full advantage of.",
+    credit: "Video by Jon",
+    color: "#32383D",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-01.png",
+        zsrc: "/content-images/spt-gt4-01-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-02.png",
+        zsrc: "/content-images/spt-gt4-02-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-03.png",
+        zsrc: "/content-images/spt-gt4-03-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-04.png",
+        zsrc: "/content-images/spt-gt4-04-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-05.png",
+        zsrc: "/content-images/spt-gt4-05-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-06.png",
+        zsrc: "/content-images/spt-gt4-06-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-07.png",
+        zsrc: "/content-images/spt-gt4-07-zoom.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-gt4-08.png",
+        zsrc: "/content-images/spt-gt4-08-zoom.png",
+      },
+    ],
   },
   {
-    "id": "3",
-    "client": "Sports Performance Tracking",
-    "location": "Melbourne",
-    "year": 2019,
-    "title": "SPT Bridge",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "color": "#333",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-3-1.png", "zsrc": "/content-images/Zoom-3-1.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-3-2.png", "zsrc": "/content-images/Zoom-3-2.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-3-3.png", "zsrc": "/content-images/Zoom-3-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-3-4.png", "zsrc": "/content-images/Zoom-3-4.png" }
-    ]
+    client: "Sports Performance Tracking",
+    location: "Melbourne",
+    year: 2019,
+    title: "SPT Bridge",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
+    color: "#CCB100", // FFDD00 too bright
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-01.png",
+        zsrc: "/content-images/spt-bridge-01.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-02.png",
+        zsrc: "/content-images/spt-bridge-02.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-03.png",
+        zsrc: "/content-images/spt-bridge-03.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-04.png",
+        zsrc: "/content-images/spt-bridge-04.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-05.png",
+        zsrc: "/content-images/spt-bridge-05.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-06.png",
+        zsrc: "/content-images/spt-bridge-06.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-07.png",
+        zsrc: "/content-images/spt-bridge-07.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-08.png",
+        zsrc: "/content-images/spt-bridge-08.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/spt-bridge-09.png",
+        zsrc: "/content-images/spt-bridge-09.png",
+      },
+    ],
   },
   {
-    "id": "4",
-    "client": "ESPN",
-    "location": "Melbourne",
-    "year": 2018,
-    "title": "ESPN FootyTips App",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "color": "#444",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-4-1.png", "zsrc": "/content-images/Zoom-4-1.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-4-2.png", "zsrc": "/content-images/Zoom-4-2.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-4-3.png", "zsrc": "/content-images/Zoom-4-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-4-4.png", "zsrc": "/content-images/Zoom-4-4.png" }
-    ]
-  }
-]
+    client: "ESPN / Disney",
+    location: "Melbourne",
+    year: 2018,
+    title: "ESPN FootyTips App",
+    description:
+      "After the completely redesigned ESPN Footytips App was released, I designed sucessful iterations and new features, creating fresh experiences for Australia's biggest tipping app!,",
+    color: "#284A85",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/espn-app-05-06.png",
+        zsrc: "/content-images/espn-app-05-06.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-app-01-02.png",
+        zsrc: "/content-images/espn-app-01-02.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-app-03-04.png",
+        zsrc: "/content-images/espn-app-03-04.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-app-07-08.png",
+        zsrc: "/content-images/espn-app-07-08.png",
+      },
+    ],
+  },
+  {
+    client: "ESPN / Disney",
+    location: "Melbourne",
+    year: 2017,
+    title: "ESPN Games",
+    description:
+      "At ESPN I designed and developed a number of new games across a variety of sports and alongside a wide range of coporate partners.",
+    color: "#3b3c3d",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/espn-games-01-02.png",
+        zsrc: "/content-images/espn-games-01-02.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-games-03-04.png",
+        zsrc: "/content-images/espn-games-03-04.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-games-05.png",
+        zsrc: "/content-images/espn-games-05.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-games-06.png",
+        zsrc: "/content-images/espn-games-06.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-games-07.png",
+        zsrc: "/content-images/espn-games-07.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/espn-games-08.png",
+        zsrc: "/content-images/espn-games-08.png",
+      },
+    ],
+  },
+];
 
 export const photography = [
   {
-    "id": "1",
-    "client": "Farah's Jewelery",
-    "location": "Melbourne",
-    "year": 2021,
-    "title": "Inventory Photograhy of Bespoke Jewely",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "color": "#555",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-1-1.png", "zsrc": "/content-images/Zoom-1-1.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-1-2.png", "zsrc": "/content-images/Zoom-1-2.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-1-3.png", "zsrc": "/content-images/Zoom-1-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-1-4.png", "zsrc": "/content-images/Zoom-1-4.png" }
-    ]
+    client: "Farah's Jewelery",
+    location: "Melbourne",
+    year: 2021,
+    title: "Inventory Photograhy of Bespoke Jewely",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
+    color: "#555",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-1-1.png",
+        zsrc: "/content-images/Zoom-1-1.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-1-2.png",
+        zsrc: "/content-images/Zoom-1-2.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-1-3.png",
+        zsrc: "/content-images/Zoom-1-3.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-1-4.png",
+        zsrc: "/content-images/Zoom-1-4.png",
+      },
+    ],
   },
   {
-    "id": "2",
-    "client": "En plein air",
-    "location": "Melbourne",
-    "year": 2020,
-    "title": "Flowers of Victoria Gardens",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "color": "#444",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-2-1.png", "zsrc": "/content-images/Zoom-2-1.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-2-2.png", "zsrc": "/content-images/Zoom-2-2.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-2-3.png", "zsrc": "/content-images/Zoom-2-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-2-4.png", "zsrc": "/content-images/Zoom-2-4.png" }
-    ]
+    client: "En plein air",
+    location: "Melbourne",
+    year: 2020,
+    title: "Flowers of Victoria Gardens",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
+    color: "#444",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-2-1.png",
+        zsrc: "/content-images/Zoom-2-1.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-2-2.png",
+        zsrc: "/content-images/Zoom-2-2.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-2-3.png",
+        zsrc: "/content-images/Zoom-2-3.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-2-4.png",
+        zsrc: "/content-images/Zoom-2-4.png",
+      },
+    ],
   },
   {
-    "id": "3",
-    "client": "Julie's Patissierie",
-    "location": "Melbourne",
-    "year": 2019,
-    "title": "Menu Photography for a French Bakery",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "color": "#333",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-3-1.png", "zsrc": "/content-images/Zoom-3-1.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-3-2.png", "zsrc": "/content-images/Zoom-3-2.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-3-3.png", "zsrc": "/content-images/Zoom-3-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-3-4.png", "zsrc": "/content-images/Zoom-3-4.png" }
-    ]
+    client: "Julie's Patissierie",
+    location: "Melbourne",
+    year: 2019,
+    title: "Menu Photography for a French Bakery",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
+    color: "#333",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-3-1.png",
+        zsrc: "/content-images/Zoom-3-1.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-3-2.png",
+        zsrc: "/content-images/Zoom-3-2.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-3-3.png",
+        zsrc: "/content-images/Zoom-3-3.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-3-4.png",
+        zsrc: "/content-images/Zoom-3-4.png",
+      },
+    ],
   },
   {
-    "id": "4",
-    "client": "Rosie",
-    "location": "Melbourne",
-    "year": 2018,
-    "title": "My Dog Rosie!",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
-    "color": "#222",
-    "slides" : [
-      { "type": "image", "ssrc": "/content-images/Slide-4-1.png", "zsrc": "/content-images/Zoom-4-1.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-4-2.png", "zsrc": "/content-images/Zoom-4-2.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-4-3.png", "zsrc": "/content-images/Zoom-4-3.png" },
-      { "type": "image", "ssrc": "/content-images/Slide-4-4.png", "zsrc": "/content-images/Zoom-4-4.png" }
-    ]
-  }
-]
+    client: "Rosie",
+    location: "Melbourne",
+    year: 2018,
+    title: "My Dog Rosie!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan blandit mi vitae efficitur. Mauris blandit efficitur molestie. Aliquam ultricies lorem ac odio sodales blandit. Nam vestibulum orci quis mi vehicula, non pulvinar risus tincidunt.",
+    color: "#222",
+    slides: [
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-4-1.png",
+        zsrc: "/content-images/Zoom-4-1.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-4-2.png",
+        zsrc: "/content-images/Zoom-4-2.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-4-3.png",
+        zsrc: "/content-images/Zoom-4-3.png",
+      },
+      {
+        type: "image",
+        ssrc: "/content-images/Slide-4-4.png",
+        zsrc: "/content-images/Zoom-4-4.png",
+      },
+    ],
+  },
+];
