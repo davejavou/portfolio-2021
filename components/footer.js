f68884dec5b5c2dc67a355b8caceafaece4ba79f
Dave <github@davecutter.com>
2021-07-22 20:28:25 +1000

    Update cutterscove icon

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faFish, faFileAlt } from '@fortawesome/free-solid-svg-icons'
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons'
+import IconIsland from './custom-icons'
 
 const iconStyle = "fill-current text-blue-light h-6 w-6 hover:text-blue-lightest"
 
@@ -9,7 +10,7 @@ export default function Footer() {
     <footer>
       <div className="flex justify-around w-3/4 lg:w-1/2 mx-auto pt-8 pb-5">
         <a target="_blank" rel="noopener" href="https://www.linkedin.com/in/davejavou/" title="Dave Cutter on LinkedIn"><Icon className={iconStyle} icon={faLinkedin} /></a>
-        <a target="_blank" rel="noopener" href="https://cutterscove.com/" title="Watch the fish at Cutter's Cove"><Icon className={iconStyle} icon={faFish} /></a>
+        <a target="_blank" rel="noopener" href="https://cutterscove.com/" title="Watch the fish at Cutter's Cove"><IconIsland className={iconStyle} /></a>
         <a target="_blank" rel="noopener" href="https://docs.google.com/document/d/1LZtQ4O3Wjzt2EAibHtvVb8OD6I7S0T7hCHDg_aqJudY/edit?usp=sharing" title="Dave Cutter's Resume on Google Docs"><Icon className={iconStyle} icon={faFileAlt} /></a>
       </div>
       <div className="p-5 text-xs text-center text-gray-lightest">Copyright &copy; Dave Cutter {new Date().getFullYear()}</div>
--- pages/chill.js
+++ pages/chill.js
@@ -1,12 +1,10 @@
-import Link from 'next/link'
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
-import { faFish } from '@fortawesome/free-solid-svg-icons'
+import IconIsland from '../components/custom-icons'
 
 export default function Chill() {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-blue-dark text-white">
       <a className="btn btn-blue w-64" rel="noopener" href="https://cutterscove.com/" title="Watch the fish at Cutter's Cove">
-        <Icon className="btn-icon" icon={faFish} />
+        <IconIsland className="btn-icon" />
         Chill.
       </a>
     </div>
