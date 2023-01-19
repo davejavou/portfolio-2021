3a1559161048c31e47685ace7c2203b6c61b80e6
Dave <github@davecutter.com>
2023-01-19 20:50:31 +1100

    Fix FOUC in Firefox

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react'
 import '../styles/globals.css'
 import '../styles/slick.1.6.0.min.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  // Prevent a FOUC in Firefox
+  // https://github.com/vercel/next.js/issues/18769
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
+
+  return (
+    <div style={{ visibility: !mounted ? 'hidden' : '' }}>
+      <Component style={{'background':'purple'}} {...pageProps} />
+    </div>
+  )
 }
