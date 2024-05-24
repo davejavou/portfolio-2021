bb5079228cf3a37c87425be345dbd9ab863472ac
davejavou <davejavou@gmail.com>
2024-05-24 06:14:04 +0000

    Update role. Add GitHub.

--- components/quickbio.js
+++ components/quickbio.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 import { faFilePdf, faFileAlt, faBookOpen } from '@fortawesome/free-solid-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default function QuickBio() {
   return (
@@ -12,8 +12,12 @@ export default function QuickBio() {
         Designer &amp; <span className="inline-block">FE Developer.</span>
       </h1>
       <h2 className="my-10 text-lg text-gray-lightest">
-        Product Designer at <a className="text-blue-light hover:text-blue-lightest" href="https://www.artprocessors.net/" title="Art Processors" target="_blank" rel="noopener">Art Processors</a>. Designer &amp; developer at <a href="https://www.espn.com.au/" className="text-blue-light hover:text-blue-lightest" title="ESPN/Disney" target="_blank" rel="noopener">ESPN/Disney</a> &amp; <a className="text-blue-light hover:text-blue-lightest" rel="noopener" href="https://sports.yahoo.com/" title="Yahoo! Sports" target="_blank">Yahoo!</a>
+        Senior Product Designer & Frontend Developer at <a className="text-blue-light hover:text-blue-lightest" href="https://factsmgt.com.au/" title="Nelnet International" target="_blank" rel="noopener">Nelnet</a>. Previously at <a href="https://www.artprocessors.net/" className="text-blue-light hover:text-blue-lightest" title="ESPN/Disney" target="_blank" rel="noopener">Art Processors</a>, <a href="https://www.espn.com.au/" className="text-blue-light hover:text-blue-lightest" title="ESPN" target="_blank" rel="noopener">ESPN</a> &amp; <a className="text-blue-light hover:text-blue-lightest" rel="noopener" href="https://sports.yahoo.com/" title="Yahoo! Sports" target="_blank">Yahoo!</a>
       </h2>
+      <Link href={`/portfolio`} title="Dave's Portfolio" className="md:hidden btn btn-blue w-full mb-2">
+        <Icon className="tab-icon" icon={faBookOpen} />
+        Portfolio
+      </Link>
       <a className="btn btn-blue w-full mb-2" target="_blank" rel="noopener" href="https://davecutter.com/portfolio/public/David-Cutter-Resume.pdf" title="Download Dave Cutter's Resume PDF">
         <Icon className="btn-icon" icon={faFilePdf} />
         Resume
@@ -22,10 +26,10 @@ export default function QuickBio() {
         <Icon className="btn-icon" icon={faLinkedin} />
         LinkedIn
       </a>
-      <Link href={`/portfolio`} title="Dave's Portfolio" className="md:hidden btn btn-blue w-full mb-2">
-        <Icon className="tab-icon" icon={faBookOpen} />
-        Portfolio
-      </Link>
+      <a className="btn btn-blue w-full mb-2" target="_blank" rel="noopener" href="https://github.com/davejavou" title="Dave Cutter on GitHub">
+        <Icon className="btn-icon" icon={faGithub} />
+        GitHub
+      </a>
     </div>
   )
 }
