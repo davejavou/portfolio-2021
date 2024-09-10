e0ce68ad088aad6f7456cfec4485b9484b646709
Dave <github@davecutter.com>
2024-09-10 16:33:37 +1000

    Fixes for local dev. Fix top-level div error (no longer on vercel)

--- next.config.js
+++ next.config.js
@@ -3,16 +3,17 @@
  */
 const nextConfig = {
   output: 'export',
-  assetPrefix: '/portfolio_2021/',
+  basePath: '/portfolio_2021', // Even when running dev, you will need to add this to the URL, ex: http://localhost:3000/portfolio_2021
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // Because my server does not support clean URLs
   trailingSlash: true,
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `dist`
-  distDir: 'dist',
+  // distDir: 'dist',
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
-import { useState, useEffect } from 'react'
 import '../styles/globals.css'
 import '../styles/slick.1.6.0.min.css'
 
 export default function App({ Component, pageProps }) {
 
-  // Prevent a FOUC in Firefox
-  // https://github.com/vercel/next.js/issues/18769
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => setMounted(true), [])
-
   return (
-    <div style={{ visibility: !mounted ? 'hidden' : '' }}>
-      <Component style={{'background':'purple'}} {...pageProps} />
-    </div>
+    <Component style={{'background':'purple'}} {...pageProps} />
   )
 }
