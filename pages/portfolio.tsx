de7f71ba49cfe2a2f0ea7e1d9dddcecba4525eb7
Dave <github@davecutter.com>
2026-02-15 18:28:22 +1100

    Ensure only one nav per device for accessibility.

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useWindowSize } from "usehooks-ts";
 import { aboutDaveCutter } from "../components/content";
 import Footer from "../components/footer";
 import Layout from "../components/layout";
@@ -7,6 +8,11 @@ import Nav, { MobileNav, NavSpacer } from "../components/nav";
 import Projects from "../components/projects";
 
 export default function Portfolio() {
+	// Determine Mobile vs Desktop for conditional Nav and Footer
+	const { width } = useWindowSize();
+	const isMobile = width < 768;
+	const isDesktop = width >= 768;
+
 	return (
 		<Layout>
 			<Head>
@@ -15,13 +21,15 @@ export default function Portfolio() {
 			</Head>
 
 			<Main>
-				<Nav />
+				{isDesktop && <Nav />}
 				<Projects />
-				<div className="md:hidden bg-gray shadow-inner">
-					<Footer />
-					<NavSpacer />
-					<MobileNav />
-				</div>
+				{isMobile && (
+					<div className="md:hidden bg-gray shadow-inner">
+						<Footer />
+						<NavSpacer />
+						<MobileNav />
+					</div>
+				)}
 			</Main>
 		</Layout>
 	);
