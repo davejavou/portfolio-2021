57e2ebbf7f9d57f3ffb345857e7ae19e80cf849c
Dave <github@davecutter.com>
2026-02-15 15:58:07 +1100

    Fix FOUC

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,25 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { AppProps } from "next/app";
+import { Nunito, Raleway } from "next/font/google";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-	const router = useRouter();
+// Prevent Font Awesome from adding its CSS since we did it manually above.
+// (See https://fontawesome.com/docs/web/use-with/react/use-with#nextjs for more details.)
+// Goal is to avoid FOUC (Flash of Unstyled Content) when using Font Awesome icons.
+config.autoAddCss = false;
 
-	// View Transitions (ignored in browsers that don't support it).
+// Load Fonts
+// Goal is to avoid FOUT (Flash of Unstyled Text) when using Google Fonts.
+// By using the next/font/google package, we can ensure that the fonts are loaded and applied before rendering the page content.
+const raleway = Raleway({ subsets: ["latin"], weight: ["600", "700"] });
+const nunito = Nunito({ subsets: ["latin"], weight: ["600", "700"] });
+
+export default function App({ Component, pageProps }: AppProps) {
+	// View Transitions
+	const router = useRouter();
 	useEffect(() => {
 		// Start a view transition before Next.js changes the route.
 		const handleRouteChange = () => {
@@ -21,5 +34,11 @@ export default function App({ Component, pageProps }: AppProps) {
 		};
 	}, [router.events]);
 
-	return <Component style={{ background: "purple" }} {...pageProps} />;
+	return (
+		<Component
+			style={{ background: "purple" }}
+			className={`${raleway.className} ${nunito.className}`}
+			{...pageProps}
+		/>
+	);
 }
